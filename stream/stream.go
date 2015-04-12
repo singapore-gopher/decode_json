721ca03a55c8a659998f69bf91ac3519cc694207
Zoltan Giber <zgiber@gmail.com>
2015-04-12 21:34:18 +0800

    added server (listening on 4001) fixed some minor reconnect issues. Fanout doesn't wait for writes(failed writes took too long, and hung the stream)

--- stream/stream.go
+++ stream/stream.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	clientId = 0
+	Port = "2015"
 )
 
 // for this json-challenge this would do the job
@@ -33,34 +33,32 @@ func (self *router) add(c io.ReadWriteCloser) {
 func (self *router) remove(id int64) {
 	self.Lock()
 	delete(self.clients, id)
+	log.Println(len(self.clients))
 	self.Unlock()
 }
 
 // send to everyone
-// waits for writes to finish
+// does not wait for writes to finish
 func (self *router) fanout(data []byte) {
 
-	var wg sync.WaitGroup
-
 	self.RLock()
-	defer self.RUnlock()
-	for clientId, c := range self.clients {
-		wg.Add(1)
+	clients := self.clients // copy the clients so we can unlock the map
+	self.RUnlock()
+
+	for clientId, c := range clients {
 		go func(clientId int64, data []byte, c io.ReadWriteCloser) {
-			defer wg.Done()
 
 			_, err := c.Write(data)
 			if err != nil {
-				log.Println(err)
-				c.Close()
 				self.remove(clientId)
+				log.Println(clientId, "disconnected:", err.Error())
+				c.Close()
 				return
 			}
 
 		}(clientId, data, c)
 	}
 
-	wg.Wait()
 	return
 
 }
@@ -79,9 +77,11 @@ func Serve(status chan int) {
 		clients: map[int64]io.ReadWriteCloser{},
 	}
 
+	// sending out the goodies.
 	go func() {
 		magicTick := time.NewTicker(1017 * time.Millisecond)
 		normalTick := time.NewTicker(57 * time.Millisecond)
+
 		for {
 			select {
 			case <-magicTick.C:
@@ -105,9 +105,10 @@ func Serve(status chan int) {
 
 	}()
 
+	// ready
 	status <- 0
 
-	l, err := net.Listen("tcp", ":2015")
+	l, err := net.Listen("tcp", ":"+Port)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -119,6 +120,8 @@ func Serve(status chan int) {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		log.Println(conn.RemoteAddr(), "connected")
 
 		r.add(conn)
 	}
